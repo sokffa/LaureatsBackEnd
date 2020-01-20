de018ec11c555975ac275efe9d8beb09522539a0
sokffa17 <s.mahmoud97@gmail.com>
2020-01-20 16:20:06 +0100

    Update views.py

--- laureats/views.py
+++ laureats/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
 from laureats.serializers import *
 from laureats.models import *
 from rest_framework.views import APIView
@@ -11,11 +10,11 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from django.core.mail import EmailMessage
 from rest_framework.response import Response
-from rest_framework import status
 from django.core.mail import EmailMessage
-from rest_framework import permissions
-
-
+from rest_framework import viewsets,status,permissions,filters
+import matplotlib.pyplot as plt
+import numpy as np
+import mpld3
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -35,6 +34,8 @@ class GroupViewSet(viewsets.ModelViewSet):
 class LaureatViewSet(viewsets.ModelViewSet):
     queryset = Laureat.objects.all()
     serializer_class = LaureatSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nom', 'prenom','cne','email','filiere__libelle','adresse__ville','adresse__pays']
     permission_classes = [permissions.IsAuthenticated]
 
 
@@ -59,17 +60,23 @@ class ProfessionViewSet(viewsets.ModelViewSet):
     queryset = Profession.objects.all()
     serializer_class = ProfessionSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['libelle']
+
     
 class EtudiantViewSet(viewsets.ModelViewSet):
     queryset = Etudiant.objects.all()
     serializer_class = EtudiantSerializer
     permission_classes = [permissions.IsAuthenticated]
-
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nom', 'prenom','cne','email','filiere__libelle','etablissement']
 
 class EmployeViewSet(viewsets.ModelViewSet):
     queryset = Employe.objects.all()
     serializer_class = EmployeSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nom', 'prenom','cne','email','filiere__libelle','profession__libelle']
 
 
 class EventViewSet(APIView):
@@ -82,16 +89,33 @@ class EventViewSet(APIView):
         return HttpResponse(liste, content_type='text/html; charset=utf-8')
 class SendEmailViewSet(APIView):
     permission_classes = [permissions.IsAuthenticated]
-
     def post(self, request, *args, **kwargs):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
             data = serializer.validated_data
-            print("LE SERIALIZER :")
-            print(data.get('emails_list'))
-            print("END OF  SERIALIZER :")
             msg = EmailMessage(data.get('subject'),
                 data.get('message'), to=data.get('emails_list'))
             msg.send()
             return Response({"success": "Sent"})
         return Response({'success': "Failed"}, status=status.HTTP_400_BAD_REQUEST)
+class StatsViewSet(APIView):
+    def get(self, request):
+        fig, ax = plt.subplots(subplot_kw=dict(facecolor='#EEEEEE'))
+        N = 100
+
+        scatter = ax.scatter(np.random.normal(size=N),
+                            np.random.normal(size=N),
+                            c=np.random.random(size=N),
+                            s=1000 * np.random.random(size=N),
+                            alpha=0.3,
+                            cmap=plt.cm.jet)
+        ax.grid(color='white', linestyle='solid')
+
+        ax.set_title("Scatter Plot (with tooltips!)", size=20)
+
+        labels = ['point {0}'.format(i + 1) for i in range(N)]
+        tooltip = mpld3.plugins.PointLabelTooltip(scatter, labels=labels)
+        mpld3.plugins.connect(fig, tooltip)
+
+        
+        return HttpResponse(mpld3.fig_to_html(fig), content_type='text/html; charset=utf-8')
